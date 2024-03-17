a889372a37246ff441ff2c49c781bbff5beb15c0
Andres E. Rojas E. <aere69@gmail.com>
2024-03-17 23:50:20 +0000

    Add memory pool and example

---
+++ LowLatency/MemoryPools/mem_pools_example.cpp
@@ -0,0 +1,28 @@
+#include "mem_pool.h"
+
+struct MyStruct {
+    int d_[3];
+};
+
+int main(int, char **){
+    using namespace common;
+
+    MemPool<double> prim_pool(50);
+    MemPool<MyStruct> struct_pool(50);
+
+    for (auto i=0; i<50; i++){
+        auto p_ret = prim_pool.allocate(i);
+        auto s_ret = struct_pool.allocate(MyStruct{i, i+1, i+2});
+
+        std::cout << "prim element : " << *p_ret << " allocated at : " << p_ret << std::endl;
+        std::cout << "struct element : " << s_ret->d_[0] << "," << s_ret->d_[1] << "," << s_ret->d_[2] << " allocated at : " << s_ret << std::endl;
+
+        if ( i % 5 == 0){
+            std::cout << "Deallocating prim element : " << *p_ret << " allocated at : " << p_ret << std::endl;
+            std::cout << "Deallocating struct element : " << s_ret->d_[0] << "," << s_ret->d_[1] << "," << s_ret->d_[2] << " allocated at : " << s_ret << std::endl;
+
+            prim_pool.deallocate(p_ret);
+            struct_pool.deallocate(s_ret);
+        }
+    }
+}
